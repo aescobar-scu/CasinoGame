5ee89bc3cbfcd6b91155242c2d6136db060c06ac
ac1229 <36652359+ac1229@users.noreply.github.com>
2018-02-28 10:21:24 -0800

    Update Player.java

--- src/casino/Player.java
+++ src/casino/Player.java
@@ -3,20 +3,32 @@ package casino;
 public class Player {
 
 	  private String name;
+	  private String nickName;
 	  private int account;
-	  Player(String name, int account){
+	  
+	  Player(String name, String nickName, int account){
 	    this.name = name;
+	    this.nickName = nickName;
 	    this.account = account;
 	  }
+	  
 	  Player(){
 	    name = "Blank";
+	    nickName = "Blank";
 	    account = 0;
 	  }
+	  
 	  public void setName(String n){
 	    this.name = n;
 	  } 
 	  public String getName(){
 	    return name;
+	  }
+	  public void setNickName(String nn) {
+		  this.nickName = nn;
+	  }
+	  public String getNickName() {
+		  return nickName;
 	  }
 	  public void setAccount(int a){
 	    this.account = a;
