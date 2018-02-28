6925c582fc32a57644dceea98256196c91e86404
lisawangjuan <32233028+lisawangjuan@users.noreply.github.com>
2018-02-27 21:51:51 -0800

    Wager class for wager information
     including wager type, payout odds, wager content and related accessors and mutators

--- src/casino/Wager.java
+++ src/casino/Wager.java
@@ -1,5 +1,75 @@
 package casino;
 
-public class Wager {
+import java.util.*;
 
+public class Wager {
+    
+    private int ownerId;
+    private String wagerType;
+    private int payoutOdds;
+    private ArrayList<Integer> wager;
+    private double wagerAmount;
+    private double winLoseAmount;
+    private final Map<String, Integer> wagerTypePayoutMap = Map.of("Straight", 35, "Split", 17, "Street", 11, "Square", 8, "Six Line", 5, "Red", 1, "Black", 1, "Dozens", 2, "Highs", 1, "Lows", 1, "Odds", 1, "Evens", 1, "Columns", 2);
+    
+    
+    // constructor 1
+    public Wager(int id) {
+        ownerId = id;
+    }
+    
+    public Wager(int id, String type, double amount, ArrayList<Integer> wager) {
+        ownerId = id;
+        wagerType = type;
+        payoutOdds = wagerTypePayoutMap.get(wagerType);
+        wagerAmount = amount;
+        wager = wager; 
+    }
+    
+    // getters
+    public int getWagerId() {
+        return ownerId;
+    }
+    
+    public String getWagerType() {
+        return wagerType;
+    }
+    
+    public int getPayoutOdds() {
+        return payoutOdds;
+    }
+    
+    public ArrayList<Integer> getWager() {
+        return wager;
+    }
+    
+    public double getWagerAmount() {
+        return wagerAmount;
+    } 
+    
+    public double getWinLoseAmount() {
+        return winLoseAmount;
+    }
+    
+    // setters
+    public void setWagerType(String type) {
+        wagerType =type;
+        payoutOdds = wagerTypePayoutMap.get(wagerType);
+    }
+    
+    public void setWager(ArrayList<Integer> wager) {
+        wager = wager;
+    }
+    
+    public void setWagerAmount(double amount) {
+        wagerAmount = amount;
+    }
+    
+    public void setWinLoseAmount(boolean win) {
+        winLoseAmount = wagerAmount * payoutOdds;
+        if (!win) {
+            winLoseAmount = -winLoseAmount;
+        }
+    }
+    
 }
